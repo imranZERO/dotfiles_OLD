59dc4f85313bafcbe98cb5d03e329cb8e316efd2
imranZERO <imran.leave@gmail.com>
2021-09-22 00:04:28 +0600

    revamp

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -1,23 +1,18 @@
 # Essentials
-git clone --depth=1 https://github.com/jpalardy/vim-slime.git && \
-git clone --depth=1 https://github.com/justinmk/vim-sneak.git && \
-git clone --depth=1 https://github.com/jiangmiao/auto-pairs.git && \
-git clone --depth=1 https://github.com/preservim/nerdcommenter.git && \
-git clone --depth=1 https://github.com/tpope/vim-surround && \
-git clone --depth=1 https://github.com/mattn/emmet-vim.git && \
-git clone --depth=1 https://github.com/junegunn/rainbow_parentheses.vim.git && \
-git clone --depth=1 https://github.com/Yggdroot/indentLine.git && \
-git clone --depth=1 https://github.com/mhinz/vim-startify && \
 git clone --depth=1 https://github.com/junegunn/fzf.git && \
 git clone --depth=1 https://github.com/junegunn/fzf.vim.git && \
+git clone --depth=1 https://github.com/tpope/vim-commentary.git && \
+git clone --depth=1 https://github.com/justinmk/vim-dirvish.vim.git && \
+git clone --depth=1 https://github.com/jpalardy/vim-slime.git && \
+git clone --depth=1 https://github.com/tpope/vim-surround && \
+git clone --depth=1 https://github.com/junegunn/rainbow_parentheses.vim.git && \
+git clone --depth=1 https://github.com/chengzeyi/multiterm.vim.git && \
 
 # opt
-git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git && \
-git clone --depth=1 https://github.com/ap/vim-css-color.git && \
 git clone --depth=1 https://github.com/sheerun/vim-polyglot.git && \
+git clone --depth=1 https://github.com/jiangmiao/auto-pairs.git && \
+git clone --depth=1 https://github.com/justinmk/vim-sneak.git && \
+git clone --depth=1 https://github.com/mattn/emmet-vim.git && \
+git clone --depth=1 https://github.com/lilydjwg/colorizer.git && \
 git clone --depth=1 https://github.com/junegunn/goyo.vim.git && \
-git clone --depth=1 https://github.com/junegunn/limelight.vim.git && \
-
-# colors
-git clone --depth=1 https://github.com/morhetz/gruvbox.git && \
 git clone --depth=1 https://github.com/rafi/awesome-vim-colorschemes.git
