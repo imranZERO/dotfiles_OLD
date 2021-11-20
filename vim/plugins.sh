4cf8ac5a62fbbd662d04c9de29fd140d2592d524
imranZERO <imran.leave@gmail.com>
2021-11-20 18:21:21 +0600

    update

--- vim/plugins.sh
+++ vim/plugins.sh
@@ -7,12 +7,13 @@ git clone --depth=1 https://github.com/jpalardy/vim-slime.git && \
 git clone --depth=1 https://github.com/tpope/vim-surround && \
 git clone --depth=1 https://github.com/junegunn/rainbow_parentheses.vim.git && \
 git clone --depth=1 https://github.com/chengzeyi/multiterm.vim.git && \
+git clone --depth=1 https://github.com/sheerun/vim-polyglot.git && \
 
 # opt
-git clone --depth=1 https://github.com/sheerun/vim-polyglot.git && \
 git clone --depth=1 https://github.com/jiangmiao/auto-pairs.git && \
 git clone --depth=1 https://github.com/justinmk/vim-sneak.git && \
 git clone --depth=1 https://github.com/mattn/emmet-vim.git && \
 git clone --depth=1 https://github.com/lilydjwg/colorizer.git && \
+git clone --depth=1 https://github.com/hauleth/asyncdo.vim.git && \
 git clone --depth=1 https://github.com/junegunn/goyo.vim.git && \
 git clone --depth=1 https://github.com/rafi/awesome-vim-colorschemes.git
