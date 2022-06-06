91e25b4ad67f2ad8160088a3985f2d02cb61b852
imranZERO <imran.leave@gmail.com>
2022-06-06 15:03:43 +0600

    lalala

--- other/cmdrc.bat
+++ other/cmdrc.bat
@@ -1,14 +1,17 @@
 :: Open your registry and go to the following key:
-:: HKEY_CURRENT_USER\Software\Microsoft\Command Processor
-:: or
+:: HKEY_CURRENT_USER\Software\Microsoft\Command Processor or
 :: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor
 :: and add a string value named AutoRun and valued with the
-:: absolute path of this file. eg: %HOME%\cmdrc.bat
+:: absolute path of this file. eg: %HOME%\cmdrc.bat.
+::
+:: You can also add shortcut target to this file. [This is preferred]
+:: eg: %windir%\system32\cmd.exe /k %HOME%\cmdrc.bat
 
 @echo off
 
 set prompt=$P$_$+$G$S
 clink.exe inject
+cls
 
 doskey v=vim $*
 doskey ls=ls -lah $*
