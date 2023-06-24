b7c93b54deed6b573451ac6df445b68c71f54f52
imranZERO <imran.leave@gmail.com>
2023-06-24 09:51:17 +0600

    noice

--- other/cmdrc.bat
+++ other/cmdrc.bat
@@ -1,5 +1,5 @@
 :: Open your registry and go to the following key:
-:: HKEY_CURRENT_USER\Software\Microsoft\Command Processor or
+:: HKEY_CURRENT_USER\Software\Microsoft\Command Processor OR
 :: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Command Processor
 :: and add a string value named AutoRun and valued with the
 :: absolute path of this file. eg: %HOME%\cmdrc.bat.
@@ -11,15 +11,28 @@
 
 set prompt=$P$_$+$G$S
 clink.exe inject
+set TERM=xterm-256color
 cls
 
 doskey ..=cd ..
 doskey ...=cd ..\..
+doskey :q=exit
 doskey v=vim $*
 doskey np=notepad $*
-doskey ls=ls -lah $*
+doskey ll=ls -lah $*
 doskey gs=git status $*
+doskey ga=git add .
 doskey gc=git clone --depth=1 $*
+doskey ggc=git commit -m "$*"
 doskey pyserve=python -m http.server $*
-doskey hss=hugo serve --noHTTPCache $*
+doskey hss=hugo serve --noHTTPCache --bind 0.0.0.0 $*
 doskey liveserve=browser-sync start --server -f -w $*
+doskey cloc=tokei $*
+
+doskey php8=D:\dev\php-8.2.3\php $*
+doskey php7=D:\dev\php-7.4.33\php $*
+doskey composer=php C:\util\composer.phar $*
+
+doskey java=D:\dev\jdk\bin\java $*
+
+doskey aastyle=C:\util\astyle --style=kr --indent=tab --convert-tabs --break-blocks --pad-oper --pad-header --unpad-paren --align-pointer=name --attach-return-type --indent-preproc-block --max-code-length=80 --break-after-logical --suffix=none $*
--- vim/pluginstall.sh
+++ vim/pluginstall.sh
@@ -15,11 +15,14 @@ $GITCMD https://github.com/chengzeyi/multiterm.vim.git
 $GITCMD https://github.com/sheerun/vim-polyglot.git
 $GITCMD https://github.com/hauleth/asyncdo.vim.git
 $GITCMD https://github.com/jpalardy/vim-slime.git
-$GITCMD https://github.com/fatih/vim-go.git
-$GITCMD https://github.com/mattn/emmet-vim.git
 $GITCMD https://github.com/habamax/vim-dir.git
 $GITCMD https://github.com/imranzero/smartpairs.vim.git
 $GITCMD https://github.com/imranzero/wintweak.gvim.git
+
+$GITCMD https://github.com/mattn/emmet-vim.git
+$GITCMD https://github.com/fatih/vim-go.git
+$GITCMD https://github.com/charlespascoe/vim-go-syntax.git
+
 # $GITCMD https://github.com/lilydjwg/colorizer.git
 # $GITCMD https://github.com/justinmk/vim-sneak.git
 # $GITCMD https://github.com/junegunn/goyo.vim.git
